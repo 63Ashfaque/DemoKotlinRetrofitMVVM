c65f075293686510c8ffadd8833c567c4f088855
ashfaque <ashfaque.mohammad@harriersys.com>
2023-04-04 16:16:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/demokotlinretrofitmvvm/view/LogInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.demokotlinretrofitmvvm.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.demokotlinretrofitmvvm.R
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
